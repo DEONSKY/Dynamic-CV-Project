ad608cea4a78ef156e4c1f4d61478594a1641739
Mert Can Yilmaz <bestrasen@hotmail.com>
2020-08-20 19:40:03 +0300

    bug fixes

--- MCY_CV3/Controllers/HomeController.cs
+++ MCY_CV3/Controllers/HomeController.cs
@@ -12,7 +12,38 @@ namespace MCY_CV3.Controllers
     public class HomeController : Controller
     {
         CV_Context db = new CV_Context();
+
+        public ActionResult HomeLayout()
+        {
+            dynamic IndexModel = new ExpandoObject();
+            IndexModel.Abouts = db.Abouts.ToList();
+            IndexModel.Admin = db.Admin.ToList();
+            List<WorkExp> WorkExpR = db.WorkExps.OrderByDescending(x => x.StartDate).ToList();
+            IndexModel.WorkExpR = WorkExpR.First();
+            IndexModel.MainCom = db.ComChannels.Where(
+                x => x.Title_EN == "Youtube" ||
+                x.Title_EN == "LinkedIn" ||
+                x.Title_EN == "Github" ||
+                x.Title_EN == "Gmail").ToList();
+            return PartialView(IndexModel);
+        }
         public ActionResult Index()
+        {
+            dynamic IndexModel = new ExpandoObject();
+            IndexModel.Abouts = db.Abouts.ToList();
+            IndexModel.Abilities = db.Abilities.ToList();
+            IndexModel.Admin = db.Admin.ToList();
+            List<WorkExp> WorkExpR = db.WorkExps.OrderByDescending(x => x.StartDate).ToList();
+            IndexModel.WorkExpR = WorkExpR.First();
+            IndexModel.MainCom = db.ComChannels.Where(
+                x => x.Title_EN == "Youtube" ||
+                x.Title_EN == "LinkedIn" ||
+                x.Title_EN == "Github" ||
+                x.Title_EN == "Gmail").ToList();
+
+            return View(IndexModel);
+        }
+        public ActionResult Communication()
         {
             dynamic IndexModel = new ExpandoObject();
             IndexModel.Abilities = db.Abilities.ToList();
@@ -20,6 +51,12 @@ namespace MCY_CV3.Controllers
             IndexModel.Admin = db.Admin.ToList();
             List<WorkExp> WorkExpR = db.WorkExps.OrderByDescending(x => x.StartDate).ToList();
             IndexModel.WorkExpR = WorkExpR.First();
+            IndexModel.MainCom = db.ComChannels.Where(
+                x => x.Title_EN == "Youtube" ||
+                x.Title_EN == "LinkedIn" ||
+                x.Title_EN == "Github" ||
+                x.Title_EN == "Gmail").ToList();
+
             return View(IndexModel);
         }
 
